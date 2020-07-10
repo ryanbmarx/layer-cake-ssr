6ba54e08cf1ed125bde6274af53b89e58e00ff4c
Ryan Marx <ryanbmarx@gmail.com>
2020-07-10 13:08:56 -0500

    Fix this

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import App from "./App.svelte";
 import data from "./content/data.json";
-
 const app = new App({
-	hydrate: true,
-	target: document.getElementById(process.env.PROJECT_SLUG),
-	props: data,
+  hydrate: true,
+  target: document.getElementById("covid-map-reopening-america"),
+  props: data,
 });
 
 export default app;
